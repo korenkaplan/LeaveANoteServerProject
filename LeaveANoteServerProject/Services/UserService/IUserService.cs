0f44878b8cd78cecb246b05b05c95f97e5f6d9df
koren_kaplan <korenkaplan96@gmail.com>
2023-07-30 12:22:34 +0300

    New: Added description comments to IUserSevice functions

--- LeaveANoteServerProject/Services/UserService/IUserService.cs
+++ LeaveANoteServerProject/Services/UserService/IUserService.cs
@@ -1,21 +1,92 @@
 ﻿using LeaveANoteServerProject.Dto_s.User_Dto_s;
 using LeaveANoteServerProject.DTO_s.User_Dto_s;
+using Microsoft.Data.SqlClient;
 
 namespace LeaveANoteServerProject.Services.UserService
 {
     public interface IUserService
     {
+        /// <summary>
+        /// Register a new user in the database.
+        /// </summary>
+        /// <param name="user">The user object containing registration details.</param>
+        /// <returns>A response with the registration status and a token if successful.</returns>
         Task<HttpResponse<object>> RegisterUser(User user);
+
+        /// <summary>
+        /// Authenticate a user and generate an access token.
+        /// </summary>
+        /// <param name="loginDto">The login information provided by the user.</param>
+        /// <returns>A response with the authentication status and the access token if successful.</returns>
         Task<HttpResponse<object>> Login(LoginDto loginDto);
+
+        /// <summary>
+        /// Retrieve a list of all users from the database.
+        /// </summary>
+        /// <returns>A response with the list of users if successful.</returns>
         Task<HttpResponse<List<User>>> GetAllUsers();
+
+        /// <summary>
+        /// Update the device token for a user in the database.
+        /// </summary>
+        /// <param name="deviceTokenUpdateDto">The object containing the user ID and updated device token.</param>
+        /// <returns>A response with the status of the token update.</returns>
         Task<HttpResponse<string>> UpdateDeviceToken(DeviceTokenUpdateDto deviceTokenUpdateDto);
+
+        /// <summary>
+        /// Update user information in the database.
+        /// </summary>
+        /// <param name="updateInformationDto">The object containing the updated user information.</param>
+        /// <returns>A response with the status of the information update.</returns>
         Task<HttpResponse<string>> UpdateUserInformation(UpdateInformationDto updateInformationDto);
+
+        /// <summary>
+        /// Update the password for a user in the database.
+        /// </summary>
+        /// <param name="updateUserPasswordDto">The object containing the user ID, old password, and new password.</param>
+        /// <returns>A response with the status of the password update.</returns>
         Task<HttpResponse<string>> UpdateUserPassword(UpdateUserPasswordDto updateUserPasswordDto);
+
+        /// <summary>
+        /// Mark a message (accident) as read for a user.
+        /// </summary>
+        /// <param name="accidentDeleteDto">The object containing the user ID and accident ID to mark as read.</param>
+        /// <returns>A response with the status of the message update.</returns>
         Task<HttpResponse<string>> DeleteAccidentFromInbox(AccidentDeleteDto accidentDeleteDto);
+
+        /// <summary>
+        /// Mark a message (accident) as deleted for a user.
+        /// </summary>
+        /// <param name="accidentDeleteDto">The object containing the user ID and accident ID to mark as deleted.</param>
+        /// <returns>A response with the status of the message update.</returns>
         Task<HttpResponse<string>> DeleteAccidentFromHistory(AccidentDeleteDto accidentDeleteDto);
+
+        /// <summary>
+        /// Get a user from the database by their car number.
+        /// </summary>
+        /// <param name="carNumber">The car number of the user to retrieve.</param>
+        /// <returns>A response with the user information if found.</returns>
         Task<HttpResponse<GetUserByCarNumberDto>> GetUserByCarNumber(string carNumber);
+
+        /// <summary>
+        /// Get a user from the database by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to retrieve.</param>
+        /// <returns>A response with the user information if found.</returns>
         Task<HttpResponse<User>> GetUserById(int id);
+
+        /// <summary>
+        /// Delete a user from the database by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to delete.</param>
+        /// <returns>A response with the status of the user deletion.</returns>
         Task<HttpResponse<string>> DeleteUserById(int id);
+
+        /// <summary>
+        /// Get a minimal user object from the database by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the user to retrieve.</param>
+        /// <returns>A response with the minimal user information if found.</returns>
         Task<HttpResponse<MinimalUserDto>> GetMinimalUserById(int id);
 
     }
--- LeaveANoteServerProject/Services/UserService/UserService.cs
+++ LeaveANoteServerProject/Services/UserService/UserService.cs
@@ -18,6 +18,7 @@ namespace LeaveANoteServerProject.Services.UserService
             _configuration = configuration;
         }
 
+        #region User Service Functions
         public async Task<HttpResponse<object>> RegisterUser(User user)
         {
             try
@@ -53,7 +54,7 @@ namespace LeaveANoteServerProject.Services.UserService
             }
             catch (Exception ex)
             {
-                return new HttpResponse<List<User>> { IsSuccessful = false,Message="Failed To Fetch All Users", Error = ex.Message ,StatusCode=500 };
+                return new HttpResponse<List<User>> { IsSuccessful = false, Message = "Failed To Fetch All Users", Error = ex.Message, StatusCode = 500 };
             }
         }
 
@@ -109,7 +110,7 @@ namespace LeaveANoteServerProject.Services.UserService
                     return new HttpResponse<string> { IsSuccessful = false, Message = "Password Update Failed", Error = "The Id was not found in the database", StatusCode = 404 };
                 }
                 bool isUpdated = user.UpdatePassword(updateUserPasswordDto);
-                if(!isUpdated) { return new HttpResponse<string> { IsSuccessful = false, Message = "Password Update Failed", Error = "old Password is incorrect", StatusCode = 400 }; }
+                if (!isUpdated) { return new HttpResponse<string> { IsSuccessful = false, Message = "Password Update Failed", Error = "old Password is incorrect", StatusCode = 400 }; }
                 await _context.SaveChangesAsync();
                 return new HttpResponse<string> { IsSuccessful = true, Message = "Password updated successfully", StatusCode = 200 };
             }
@@ -154,7 +155,7 @@ namespace LeaveANoteServerProject.Services.UserService
             {
                 return new HttpResponse<User> { IsSuccessful = false, Message = "Failed to fetch user", Error = ex.Message, StatusCode = 500 };
             }
-       
+
         }
 
         public async Task<HttpResponse<MinimalUserDto>> GetMinimalUserById(int id)
@@ -173,7 +174,7 @@ namespace LeaveANoteServerProject.Services.UserService
             {
                 return new HttpResponse<MinimalUserDto> { IsSuccessful = false, Message = "Failed to fetch user", Error = ex.Message, StatusCode = 500 };
             }
-        
+
         }
 
         public async Task<HttpResponse<string>> DeleteAccidentFromInbox(AccidentDeleteDto accidentDeleteDto)
@@ -249,66 +250,78 @@ namespace LeaveANoteServerProject.Services.UserService
         }
 
         public async Task<HttpResponse<string>> UpdateUserInformation(UpdateInformationDto updateInformationDto)
-    {
-        try
         {
-            var user = await _context.Users.FindAsync(updateInformationDto.Id);
-            if (user == null)
+            try
             {
-                return new HttpResponse<string> { IsSuccessful = false, Message = "Information Update Failed", Error = "The Id was not found in the database", StatusCode = 404 };
+                var user = await _context.Users.FindAsync(updateInformationDto.Id);
+                if (user == null)
+                {
+                    return new HttpResponse<string> { IsSuccessful = false, Message = "Information Update Failed", Error = "The Id was not found in the database", StatusCode = 404 };
+                }
+                user.UpdateInformation(updateInformationDto);
+                await _context.SaveChangesAsync();
+                return new HttpResponse<string> { IsSuccessful = true, Message = "Information updated successfully", StatusCode = 200 };
             }
-            user.UpdateInformation(updateInformationDto);
-            await _context.SaveChangesAsync();
-            return new HttpResponse<string> { IsSuccessful = true, Message = "Information updated successfully", StatusCode = 200 };
-        }
-        catch (DbUpdateException ex)
-        {
-            if (ex.InnerException is SqlException sqlException && IsDuplicateKeyError(sqlException))
+            catch (DbUpdateException ex)
             {
-                string errorMessage = GetDuplicateKeyErrorMessage(sqlException);
-                return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = errorMessage, StatusCode = 400 };
+                if (ex.InnerException is SqlException sqlException && IsDuplicateKeyError(sqlException))
+                {
+                    string errorMessage = GetDuplicateKeyErrorMessage(sqlException);
+                    return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = errorMessage, StatusCode = 400 };
+                }
+
+                return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = ex.Message, StatusCode = 500 };
             }
+            catch (Exception ex)
+            {
+                return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = ex.Message, StatusCode = 500 };
+            }
+        } 
+        #endregion
 
-            return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = ex.Message, StatusCode = 500 };
-        }
-        catch (Exception ex)
-        {
-            return new HttpResponse<string> { IsSuccessful = false, Message = "Failed to update information", Error = ex.Message, StatusCode = 500 };
-        }
-    }
-
+        #region Index Duplication Error Functions 
+        /// <summary>
+        /// Check if the given exception indicates a duplicate key (unique index violation) error in SQL Server.
+        /// </summary>
+        /// <param name="ex">The SQL exception to check.</param>
+        /// <returns>True if the exception indicates a duplicate key error; otherwise, false.</returns>
         private bool IsDuplicateKeyError(SqlException ex)
-    {
-        // SQL Server error code for unique index violation is 2601
-        // You can also check for error number 2627, which corresponds to the same issue.
-        return ex.Number == 2601 || ex.Number == 2627;
-    }
+        {
+            // SQL Server error code for unique index violation is 2601
+            // You can also check for error number 2627, which corresponds to the same issue.
+            return ex.Number == 2601 || ex.Number == 2627;
+        }
 
+        /// <summary>
+        /// Get the error message for a duplicate key (unique index violation) error.
+        /// And return it in a user readable form.
+        /// </summary>
+        /// <param name="ex">The SQL exception containing the error details.</param>
+        /// <returns>The error message indicating which field caused the duplicate key issue.</returns>
         private string GetDuplicateKeyErrorMessage(SqlException ex)
         {
             // Extract the index name from the exception message to indicate which field caused the issue.
-          
+
             int startIndex = ex.Message.IndexOf('\'');
             int endIndex = ex.Message.LastIndexOf('\'');
             string indexName = ex.Message.Substring(startIndex + 1, endIndex - startIndex - 1);
 
             // You can create a mapping to convert index names to more user-friendly field names if needed.
             string field;
-                if( indexName.Contains("CarNumber"))
-                    field = "car number";
-                 else if (indexName.Contains("PhoneNumber"))
-                    field = "phone number";
-                 else if (indexName.Contains("DeviceToken"))
-                    field = "device token";
-                 else if (indexName.Contains("Email"))
-                    field = "email";
-                 else
-                    field = "field";
+            if (indexName.Contains("CarNumber"))
+                field = "car number";
+            else if (indexName.Contains("PhoneNumber"))
+                field = "phone number";
+            else if (indexName.Contains("DeviceToken"))
+                field = "device token";
+            else if (indexName.Contains("Email"))
+                field = "email";
+            else
+                field = "field";
 
             return $"This {field} is aleady in use by another user";
-        }
-
-
+        } 
+        #endregion
 
     }
 
