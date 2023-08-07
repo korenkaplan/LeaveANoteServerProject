a39dc23e1c4596f7ae5e65110151677536faba00
koren_kaplan <korenkaplan96@gmail.com>
2023-08-07 11:57:47 +0300

    Update naming in user-Secrets

--- LeaveANoteServerProject/Program.cs
+++ LeaveANoteServerProject/Program.cs
@@ -59,8 +59,11 @@ if (builder.Environment.IsProduction())
 
 if (builder.Environment.IsDevelopment())
 {
-    string conString = builder.Configuration["ConnectionsStrings:AzureSql"];
-    string jwtToken = builder.Configuration["Authentication:Schemes:Bearer:SigningKeys:0:Value"];
+    string conString = builder.Configuration["ConnectionsStrings:defaultConnection"];
+    string conString2 = builder.Configuration["ConnectionsStrings:AzureSql"];
+    Log.Information(conString);
+    //string jwtToken = builder.Configuration["Authentication:Schemes:Bearer:SigningKeys:0:Value"];
+    string jwtToken = builder.Configuration["JWTKEY"];
     builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(conString));
     Token.JWTKEY = jwtToken;
 }
